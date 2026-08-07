10754436793fbefca1711b50500d17b021a24543
Flunxyz <299626154+Flunxyz@users.noreply.github.com>
2026-08-07 19:48:00 +0300

    Add non-functioning UI, change operators to arrow functions, add operate function

--- javascript.js
+++ javascript.js
@@ -5,19 +5,19 @@
 	Create a function that divides selected nubmers from each other.
 
 */
-const add = function (number1, number2) {
-	return number1 + number2;
-};
 
-const subtract = function (number1, number2) {
-	return number1 - number2;
-};
+const add  = ((number1, number2) => number1 + number2);
+const subtract  = ((number1, number2) => number1 - number2);
+const multiply  = ((number1, number2) => number1 * number2);
+const divide  = ((number1, number2) => number1 / number2);
+console.log(divide(2, 5))
 
-const multiply = function (number1, number2) {
-	return number1 * number2;
-};
+let userNumber1 = 2;
+let userNumber2 = 5;
+let operator = add;
 
-const divide = function (number1, number2) {
-	return number1 / number2;
+const operate = function (calculation) {
+	return calculation = operator(userNumber1, userNumber2);
 };
-console.log(divide(20, 5))
+console.log(operate())
+
