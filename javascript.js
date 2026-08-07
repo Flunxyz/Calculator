b278adb5c39884ad9bf1f15e392e29bb6d0dee99
Flunxyz <299626154+Flunxyz@users.noreply.github.com>
2026-08-07 17:29:34 +0300

    Create operator functions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+/* 1. 
+	Create a function that adds selected numbers together.
+	Create a function that subtracts selected numbers from each other.
+	Create a function that multiplies selected numbers together.
+	Create a function that divides selected nubmers from each other.
+
+*/
+const add = function (number1, number2) {
+	return number1 + number2;
+};
+
+const subtract = function (number1, number2) {
+	return number1 - number2;
+};
+
+const multiply = function (number1, number2) {
+	return number1 * number2;
+};
+
+const divide = function (number1, number2) {
+	return number1 / number2;
+};
+console.log(divide(20, 5))
