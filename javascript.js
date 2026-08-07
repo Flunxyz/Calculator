37af10b895a01c57cb8d91b6627b216d13b982de
Flunxyz <299626154+Flunxyz@users.noreply.github.com>
2026-08-07 20:39:33 +0300

    Add event listener for numbers and add a display for the calculator

--- javascript.js
+++ javascript.js
@@ -4,16 +4,26 @@
 	Create a function that multiplies selected numbers together.
 	Create a function that divides selected nubmers from each other.
 
+	5.
 */
+
 
 const add  = ((number1, number2) => number1 + number2);
 const subtract  = ((number1, number2) => number1 - number2);
 const multiply  = ((number1, number2) => number1 * number2);
 const divide  = ((number1, number2) => number1 / number2);
-console.log(divide(2, 5))
 
-let userNumber1 = 2;
-let userNumber2 = 5;
+const displayP = document.querySelector('#displayP')
+let buttonNumbers = document.querySelectorAll('.num');
+
+let userNumber1 = buttonNumbers.forEach((num) => 
+	num.addEventListener('click', () => {
+		displayP.textContent = num.textContent;
+		console.log(num.textContent);
+	})
+);
+
+let userNumber2;
 let operator = add;
 
 const operate = function (calculation) {
