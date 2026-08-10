f866e3709d16c7493c5727fb5b98278a3b30b56f
Flunxyz <299626154+Flunxyz@users.noreply.github.com>
2026-08-10 18:39:55 +0300

    Add clear button

--- javascript.js
+++ javascript.js
@@ -1,21 +1,4 @@
 /* 1. 
-	Create a function that adds selected numbers together.
-	Create a function that subtracts selected numbers from each other.
-	Create a function that multiplies selected numbers together.
-	Create a function that divides selected nubmers from each other.
-
-	6. Create a variable for the display.
-	Add the first number text content to it until an operation has been clicked.
-	When an operator has been clicked, set the operator vaiable value to the operator type.
-
-	But when trying to do consecutive calculations without clicking equals
-	Looks like the first number gets deleted and the new number gets added onto it as a string.
-	Might be due to the first calculation never happening in the first place when another
-	operator has been clicked.
-
-	The first equation still seems to let a string in somwehere, when when using the add operator wihtout equals.
-	The display is also not showing the calculation after doing multiple equations without using equals.
-
 
 
 */
@@ -31,31 +14,40 @@ const operations = document.querySelectorAll('.operation');
 const buttons = document.querySelectorAll('button');
 
 let newNumber1 = [];
-let functionRan = [];
+let functionRan = false;
 let number1 = [];
-let operator;
+let operator = [];
 let display = [];
 let number2 = [];
 let amount = 0;
 
+
 const calculator = buttons.forEach((button) =>
 	button.addEventListener('click', (buttonType)=> {
-		if(button.id === 'equals') {
+		console.log(`number1: ${number1} number 2: ${number2} operator: ${operator}`)
+		if(button.id === 'clear') {
+			newNumber1 = [];
+			functionRan = false;
+			number1 = [];
+			operator = [];
+			display = [];
+			number2 = [];
+			amount = 0
+			displayText.textContent = [];
+		}
+		if(button.id === 'equals' ||
+			 button.id !== 'equals' && amount === 1 && button.className === 'operation') {
 				number1 = Number(number1);
 				number2 = Number(number2);
 				calculateNumber();
-		} else if (button.id !== 'equals' && amount === 1 && button.className === 'operation') {
-				number1 = Number(number1)
-				number2 = Number(number2);
-				calculateNumber();		
-		}
-
+		} 
+	
 		if (button.className === 'num' && amount === 0) {
 			display += button.textContent;
 			number1 += button.textContent;
 			displayText.textContent = number1;
 			
-		} else if (button.className === 'operation') {	
+		} if (button.className === 'operation') {	
 				amount = 1;
 				let type = button.textContent;
 					if (type === '+' ) {
@@ -68,29 +60,30 @@ const calculator = buttons.forEach((button) =>
 						operator = divide;
 					} 
 		} else if (button.className === 'num' && amount === 1 
-				|| button.className === 'operation' && amount === 1 ) {
-					if (functionRan === true) {
-						number2 += button.textContent;
-						displayText.textContent = number2;
-							number1 = newNumber1;
-							number1 = Number(number1);
-							number2 = Number(number2);
-							if(amount === 2) {
-							amount = 1;
-							calculateNumber();
-							}
-					} else {
-						number2 += button.textContent;
-						displayText.textContent = number2;
-					}
+			|| button.className === 'operation' && amount === 1 ) {
+				if (functionRan === true) {
+					number2 += button.textContent;
+					displayText.textContent = number2;
+						number1 = newNumber1;
+						number1 = Number(number1);
+						number2 = Number(number2);
+						
+				} else {
+					number2 += button.textContent;
+					displayText.textContent = number2;
+				}
 		} 
 	}) 
 );
 function calculateNumber (number) {
 	functionRan = true;
 	number = operator(number1, number2);
+	if(number.toString().length > 4) {
+		number = number.toFixed(4)
+	}
 	displayText.textContent = number;
 	newNumber1 = number;
+	number1 = [];
 	number2 = [];
 }
 
